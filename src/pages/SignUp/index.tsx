2e11a132cbb7cc6f3122a8196ae4fdc5b4efcb35
Camila Moura <camilapaleo@gmail.com>
2020-07-01 17:17:19 +0200

    added unform in SignIn an SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from "react-native";
 import Icon from "react-native-vector-icons/Feather";
 import { useNavigation } from "@react-navigation/native";
+import { Form } from "@unform/mobile";
+import { FormHandles } from "@unform/core";
 
 import Input from "../../components/Input";
 import Button from "../../components/Button";
@@ -15,6 +17,7 @@ import logoImg from "../../assets/logo.png";
 import { Container, Title, BackToSignIn, BackToSignInText } from "./styles";
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   return (
@@ -33,11 +36,26 @@ const SignUp: React.FC = () => {
             <View>
               <Title>Register</Title>
             </View>
-            <Input name="name" icon="user" placeholder="Name" />
-            <Input name="email" icon="mail" placeholder="Email" />
-            <Input name="password" icon="lock" placeholder="Password" />
+            <Form
+              ref={formRef}
+              onSubmit={(data) => {
+                console.log(FormData);
+              }}
+            >
+              <Input name="name" icon="user" placeholder="Name" />
 
-            <Button onPress={() => {}}>Enter</Button>
+              <Input name="email" icon="mail" placeholder="Email" />
+
+              <Input name="password" icon="lock" placeholder="Password" />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Enter
+              </Button>
+            </Form>
           </Container>
         </ScrollView>
         <BackToSignIn onPress={() => navigation.goBack()}>
